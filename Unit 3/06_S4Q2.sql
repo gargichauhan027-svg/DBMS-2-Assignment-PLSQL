3a5734b727b8a8f0480409e935bff44884d55f04
gargichauhan027-svg <gargichauhan027@gmail.com>
2026-08-24 08:55:22 +0530

    Add Unit 3 Trace Q2

---
+++ Unit 3/06_S4Q2.sql
@@ -0,0 +1,7 @@
+-- Gargi Chauhan | Roll No. 06
+-- Error: PLS-00103 / cursor parameter declarations cannot specify a size.
+-- Correct line:
+SET SERVEROUTPUT ON;
+DECLARE CURSOR c_mem(p_course VARCHAR2) IS SELECT member_name FROM lib_member WHERE course=p_course;
+BEGIN FOR r IN c_mem('MCA') LOOP DBMS_OUTPUT.PUT_LINE(r.member_name); END LOOP; END;
+/
